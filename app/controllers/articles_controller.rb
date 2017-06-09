e50d4eb3bd5b83bd3d001a4eec9340f148fec1ec
Yukiko Kinoshita <yukiko-kinoshita@dcl.cs.waseda.ac.jp>
2017-06-09 17:24:00 +0000

    use before_action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
     
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+    
     def index
         @articles = Article.all
     end
     
     def show
-        @article = Article.find(params[:id])
     end
     
     def new
@@ -22,11 +23,9 @@ class ArticlesController < ApplicationController
     end
     
     def edit
-        @article = Article.find(params[:id])
     end
     
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             redirect_to articles_path
         else
@@ -35,7 +34,6 @@ class ArticlesController < ApplicationController
     end
     
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to articles_path
     end
@@ -45,6 +43,10 @@ class ArticlesController < ApplicationController
     
     def article_params
         params[:article].permit(:title, :content)
+    end
+    
+    def set_article
+        @article = Article.find(params[:id])
     end
 
 end
