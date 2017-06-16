5a0beef9fadf21552493f29ef0372be33aa6397d
Yukiko Kinoshita <yukiko-kinoshita@dcl.cs.waseda.ac.jp>
2017-06-16 05:05:25 +0000

    change error message

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
     has_many :comments
-    validates :title, presence: { message: "を入力してください" }
-    validates :content, presence: { message: "を入力してください" }
+    validates :title, presence: true
+    validates :content, presence: true
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
-  validates :content, presence: { message: "を入力してください" }
+  validates :content, presence: true
 end
